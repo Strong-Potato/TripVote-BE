ba09da6db2dadc52f7c13d97414af0fad2fafc3f
happymink <minkyun@kakao.com>
2023-12-28 17:29:28 +0900

    [Init] 프로젝트 기초 설정

---
+++ src/main/java/fc/be/tripvote/TripVoteApplication.java
@@ -0,0 +1,13 @@
+package fc.be.tripvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripVoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripVoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fc/be/tripvote/TripVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package fc.be.tripvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripVoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
