45c4352b3b9bbe68a40650eaa777ed3b1af1ee9f
happymink <minkyun@kakao.com>
2023-12-30 15:56:01 +0900

    [Refactor] VoteStatus 수정
    `#` RequiredArgsConstructor -> AllArgsConstructor 수정
    `#` field Naming, text -> description 수정

--- src/main/java/fc/be/tripvote/domain/space/vo/VoteStatus.java
+++ src/main/java/fc/be/tripvote/domain/space/vo/VoteStatus.java
@@ -1,13 +1,13 @@
 package fc.be.tripvote.domain.space.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public enum VoteStatus {
     VOTING("투표중"),
     DONE("투표완료");
 
-    private final String text;
+    private final String description;
 }
