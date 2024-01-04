a0b317f00f2924c6562e9903fd6eb5993e897bf1
Denny-Ko <donghar@naver.com>
2024-01-05 00:00:17 +0900

    [Fix] ErrorCode Lombok 의 생성자 사용
    아래의 기능이 추가됨
    `#` 전체 생성자: ExternalServiceErrorCode

--- app/src/main/java/fc/be/global/exception/ExternalServiceErrorCode.java
+++ app/src/main/java/fc/be/global/exception/ExternalServiceErrorCode.java
@@ -1,17 +1,13 @@
 package fc.be.global.exception;
 
-
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum ExternalServiceErrorCode {
     MYSQL_CONNECTION_FAIL("INTERNAL_SERVER_ERROR", "잠시 서비스가 중단되었습니다.");
 
     private final String title;
     private final String detail;
-
-    ExternalServiceErrorCode(String title, String detail) {
-        this.title = title;
-        this.detail = detail;
-    }
 }
