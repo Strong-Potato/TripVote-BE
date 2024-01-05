e8bd725686a4587b5674b81a53f926395db806c3
Ok JaeOok <okjaeook98@gmail.com>
2024-01-05 18:52:16 +0900

    [Feat] TourAPI 반환 Mapper 추가
    반환된 dto를 도메인 객체에 맞게 매핑

---
+++ openapi/src/main/java/fc/be/tourapi/dto/mapper/AreaBasedSyncMapper.java
@@ -0,0 +1,37 @@
+package fc.be.tourapi.dto.mapper;
+
+import fc.be.tourapi.constant.ContentTypeId;
+import fc.be.domain.place.Location;
+import fc.be.domain.place.Place;
+import fc.be.tourapi.dto.form.same_property.area_based_sync_list1.AreaBasedSyncList1Response;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+import static fc.be.tourapi.TourAPIProperties.formattter;
+
+@Component
+public class AreaBasedSyncMapper implements TourAPIMapper<Place, AreaBasedSyncList1Response.Item> {
+
+    public Place generate(AreaBasedSyncList1Response.Item item) {
+        return Place.builder()
+                .id(Integer.parseInt(item.contentid()))
+                .contentTypeId(ContentTypeId.of(Integer.parseInt(item.contenttypeid())))
+                .title(item.title())
+                .location(Location.builder()
+                        .address(item.addr1())
+                        .addressDetail(item.addr2())
+                        .phone(item.tel())
+                        .areaCode(Integer.parseInt(item.areacode()))
+                        .sigunguCode(Integer.parseInt(item.sigungucode()))
+                        .zipCode(Integer.parseInt(item.zipcode()))
+                        .latitude(Double.parseDouble(item.mapy()))
+                        .longitude(Double.parseDouble(item.mapx()))
+                        .build())
+                .thumbnail(item.firstimage())
+                .originalImage(item.firstimage2())
+                .createdTime(LocalDateTime.parse(item.createdtime(), formattter))
+                .modifiedTime(LocalDateTime.parse(item.modifiedtime(), formattter))
+                .build();
+    }
+}
---
+++ openapi/src/main/java/fc/be/tourapi/dto/mapper/TourAPIMapper.java
@@ -0,0 +1,18 @@
+package fc.be.tourapi.dto.mapper;
+
+/**
+ * TourAPI의 응답을 도메인 객체로 변환하는 인터페이스
+ *
+ * @param <T> 장소 상위 도메인 객체. Place
+ * @param <I> TourAPI의 응답 객체
+ */
+public interface TourAPIMapper<T, I> {
+
+    /**
+     * TourAPI의 응답 객체를 도메인 객체로 변환한다.
+     *
+     * @param item TourAPI의 응답 객체
+     * @return 도메인 객체
+     */
+    T generate(I item);
+}
