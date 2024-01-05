bb8b0c5744d14d20e7040ecf55c660d170c32887
Ok JaeOok <okjaeook98@gmail.com>
2024-01-05 18:49:39 +0900

    [Feat] TourAPI 반환 dto 폼 추가 - diff property
    각 api 별 응답 폼 프로퍼티가 다른 것들만 모아두었음

---
+++ openapi/src/main/java/fc/be/tourapi/dto/form/diff_property/detail_intro1/item/AccommodationItemDetailIntro.java
@@ -0,0 +1,40 @@
+package fc.be.tourapi.dto.form.diff_property.detail_intro1.item;
+
+
+import fc.be.tourapi.dto.form.diff_property.detail_intro1.Item;
+
+public record AccommodationItemDetailIntro(
+        String contentid, // 콘텐츠 ID
+        String contenttypeid, // 콘텐츠 타입 ID
+        String goodstay, // 굿스테이 여부
+        String benikia, // 베니키아 여부
+        String hanok, // 한옥 여부
+        String roomcount, // 객실 수
+        String roomtype, // 객실 타입
+        String refundregulation, // 환불 규정
+        String checkintime, // 체크인 시간 ★
+        String checkouttime, // 체크아웃 시간 ★
+        String chkcooking, // 취사 가능 여부
+        String seminar, // 세미나실 여부
+        String sports, // 스포츠 시설 여부
+        String sauna, // 사우나 여부
+        String beauty, // 뷰티 시설 여부
+        String beverage, // 음료 시설 여부
+        String karaoke, // 노래방 여부
+        String barbecue, // 바베큐 시설 여부
+        String campfire, // 캠프파이어 시설 여부
+        String bicycle, // 자전거 대여 여부
+        String fitness, // 피트니스 시설 여부
+        String publicpc, // 공용 PC 여부
+        String publicbath, // 공용 목욕탕 여부
+        String subfacility, // 부대 시설
+        String foodplace, // 식사 장소
+        String reservationurl, // 예약 URL ★
+        String pickup, // 픽업 서비스 여부
+        String infocenterlodging, // 문의 및 안내 (숙박)
+        String parkinglodging, // 주차 시설 (숙박) ★
+        String reservationlodging, // 예약 정보 (숙박)
+        String scalelodging, // 규모 (숙박)
+        String accomcountlodging // 수용 인원 (숙박)
+) implements Item {
+}
---
+++ openapi/src/main/java/fc/be/tourapi/dto/form/diff_property/detail_intro1/item/LeportsItemDetailIntro.java
@@ -0,0 +1,23 @@
+package fc.be.tourapi.dto.form.diff_property.detail_intro1.item;
+
+import fc.be.tourapi.dto.form.diff_property.detail_intro1.Item;
+
+public record LeportsItemDetailIntro(
+        String contentid, // 콘텐츠 ID
+        String contenttypeid, // 콘텐츠 타입 ID
+        String openperiod, // 개장 기간 ★
+        String reservation, // 예약 정보
+        String infocenterleports, // 문의 및 안내 (레포츠) ★
+        String scaleleports, // 규모 (레포츠)
+        String accomcountleports, // 수용 인원 (레포츠)
+        String restdateleports, // 쉬는 날 (레포츠) ★
+        String usetimeleports, // 이용 시간 (레포츠) ★
+        String usefeeleports, // 이용 요금 (레포츠) ★
+        String expagerangeleports, // 체험 가능 연령 (레포츠)
+        String parkingleports, // 주차 시설 (레포츠) ★
+        String parkingfeeleports, // 주차 요금 (레포츠)
+        String chkbabycarriageleports, // 유모차 대여 여부 (레포츠)
+        String chkpetleports, // 애완동물 동반 가능 여부 (레포츠)
+        String chkcreditcardleports // 신용카드 가능 여부 (레포츠)
+) implements Item {
+}
