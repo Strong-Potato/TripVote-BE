25125bb68c6d5c067ab4e5350438498cc371358b
Ok JaeOok <okjaeook98@gmail.com>
2024-01-05 22:48:31 +0900

    [Refactor] TourAPIMapper 수정
    T extends Place

--- openapi/src/main/java/fc/be/tourapi/dto/mapper/TourAPIMapper.java
+++ openapi/src/main/java/fc/be/tourapi/dto/mapper/TourAPIMapper.java
@@ -1,12 +1,14 @@
 package fc.be.tourapi.dto.mapper;
 
+import fc.be.domain.place.Place;
+
 /**
  * TourAPI의 응답을 도메인 객체로 변환하는 인터페이스
  *
  * @param <T> 장소 상위 도메인 객체. Place
  * @param <I> TourAPI의 응답 객체
  */
-public interface TourAPIMapper<T, I> {
+public interface TourAPIMapper<T extends Place, I> {
 
     /**
      * TourAPI의 응답 객체를 도메인 객체로 변환한다.
