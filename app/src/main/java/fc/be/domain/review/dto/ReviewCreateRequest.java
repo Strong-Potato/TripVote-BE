6a72b0c76779b2ca207af933006d56c580c43cc8
happymink <minkyun@kakao.com>
2024-01-07 23:42:17 +0900

    [Refactor] ReviewDTO 및 ReviewService 수정
    `+` Review Request Convert
    `#` 위에 따른 Service 변경

--- app/src/main/java/fc/be/domain/review/dto/ReviewCreateRequest.java
+++ app/src/main/java/fc/be/domain/review/dto/ReviewCreateRequest.java
@@ -1,12 +1,13 @@
 package fc.be.domain.review.dto;
 
+import fc.be.domain.member.entity.Member;
+import fc.be.domain.place.Location;
 import fc.be.domain.place.Place;
 import fc.be.domain.review.entity.Review;
 import fc.be.tourapi.constant.ContentTypeId;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
+import fc.be.tourapi.dto.bone.LocationDTO;
+import fc.be.tourapi.dto.bone.PlaceDTO;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -23,14 +24,44 @@ public record ReviewCreateRequest(
         @PastOrPresent LocalDate visitedAt
 ) {
 
-    public Review toEntity(ReviewCreateRequest reviewCreateRequest){
+    // todo [Review] Member 관련 Security 확인 되는 대로 dto 수정 하겠음 - 민균
+    public Review to(ReviewCreateRequest reviewCreateRequest, Member member) {
         return Review.builder()
-                .member()
-                .place()
-                .visitedAt()
-                .rating()
-                .images()
-                .content()
+                .member(member)
+                .place(convertPlace(reviewCreateRequest.placeDTO))
+                .visitedAt(reviewCreateRequest.visitedAt)
+                .rating(reviewCreateRequest.rating)
+                .content(reviewCreateRequest.content)
+                .images(images)
                 .build();
     }
+
+    private Place convertPlace(PlaceDTO placeDTO){
+        return Place.builder()
+                .id(placeDTO.getId())
+                .contentTypeId(placeDTO.getContentTypeId())
+                .title(placeDTO.getTitle())
+                .location(convertLocation(placeDTO.getLocationDTO()))
+                .thumbnail(placeDTO.getThumbnail())
+                .originalImage(placeDTO.getOriginalImage())
+                .createdTime(placeDTO.getCreatedTime())
+                .modifiedTime(placeDTO.getModifiedTime())
+                .gallery(placeDTO.getGallery()) //todo gallery List 확인 필요
+                .build();
+    }
+
+    private Location convertLocation(LocationDTO locationDTO){
+        return Location.builder()
+                .address(locationDTO.getAddress())
+                .addressDetail(locationDTO.getAddressDetail())
+                .phone(locationDTO.getPhone())
+                .areaCode(locationDTO.getAreaCode())
+                .sigunguCode(locationDTO.getSigunguCode())
+                .zipCode(locationDTO.getZipCode())
+                .latitude(locationDTO.getLatitude())
+                .longitude(locationDTO.getLongitude())
+                .build();
+    }
+
+
 }
