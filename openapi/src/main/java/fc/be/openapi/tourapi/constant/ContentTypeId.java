86df84273aff07c7079f090721f2022bca0803cd
Ok JaeOok <okjaeook98@gmail.com>
2024-01-07 20:25:28 +0900

    [Style] TourAPI 오류 처리 체계 반영 - constant
    `#` TourAPIException -> TourAPIError

--- openapi/src/main/java/fc/be/openapi/tourapi/constant/ContentTypeId.java
+++ openapi/src/main/java/fc/be/openapi/tourapi/constant/ContentTypeId.java
@@ -1,23 +1,28 @@
 package fc.be.openapi.tourapi.constant;
 
+import fc.be.openapi.tourapi.exception.TourAPIError;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static fc.be.openapi.config.TourAPIProperties.*;
+import static fc.be.openapi.tourapi.exception.TourAPIErrorCode.NO_CONTENTTPYEID;
+import static java.lang.Integer.parseInt;
 
+@Slf4j
 @Getter
 @AllArgsConstructor
 public enum ContentTypeId {
-    SPOT(Integer.parseInt(SPOT_STR)),
-    FACILITY(Integer.parseInt(FACILITY_STR)),
-    FESTIVAL(Integer.parseInt(FESTIVAL_STR)),
-    LEPORTS(Integer.parseInt(LEPORTS_STR)),
-    ACCOMMODATION(Integer.parseInt(ACCOMMODATION_STR)),
-    SHOP(Integer.parseInt(SHOP_STR)),
-    RESTAURANT(Integer.parseInt(RESTAURANT_STR));
+    SPOT(parseInt(SPOT_STR)),
+    FACILITY(parseInt(FACILITY_STR)),
+    FESTIVAL(parseInt(FESTIVAL_STR)),
+    LEPORTS(parseInt(LEPORTS_STR)),
+    ACCOMMODATION(parseInt(ACCOMMODATION_STR)),
+    SHOP(parseInt(SHOP_STR)),
+    RESTAURANT(parseInt(RESTAURANT_STR));
 
     private final int id;
 
@@ -33,7 +38,9 @@ public enum ContentTypeId {
         if (BY_ID.containsKey(id)) {
             return BY_ID.get(id);
         }
-        throw new IllegalArgumentException("존재하지 않는 ContentTypeId 입니다.");
+
+        new TourAPIError(NO_CONTENTTPYEID);
+        return null;
     }
 
 }
