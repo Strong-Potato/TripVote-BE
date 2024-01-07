610e2c3a34e053cd7f47532f190e1ce5e116e7df
Denny-Ko <donghar@naver.com>
2024-01-08 02:53:58 +0900

    [Fix] Member 엔티티 변경
    `#` 불필요한 column 제거: Member.name
    `+` 필요한 column 추가: Member.profile, Member.provider, Member.providedId
    `+` AuthProvider

--- app/src/main/java/fc/be/app/domain/member/entity/Member.java
+++ app/src/main/java/fc/be/app/domain/member/entity/Member.java
@@ -1,14 +1,9 @@
 package fc.be.app.domain.member.entity;
 
+import fc.be.app.domain.member.vo.AuthProvider;
 import fc.be.app.global.entity.BaseTimeEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.Comment;
 
@@ -30,9 +25,26 @@ public class Member extends BaseTimeEntity {
     @Comment("회원 비밀번호")
     private String password;
 
-    @Comment("회원 실명")
-    private String name;
-
     @Comment("회원 닉네임")
     private String nickname;
+
+    @Comment("프로필 이미지")
+    private String profile;
+
+    @Comment("회원가입 경로")
+    @Enumerated(EnumType.STRING)
+    private AuthProvider provider;
+
+    @Comment("회원정보 제공자가 제공하는 id")
+    private String providedId;
+
+    @Builder
+    private Member(String email, String password, String nickname, String profile, AuthProvider provider, String providedId) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.provider = provider;
+        this.providedId = providedId;
+        this.profile = profile;
+    }
 }
