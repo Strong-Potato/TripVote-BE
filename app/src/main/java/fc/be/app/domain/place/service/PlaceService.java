93da45120ea5a212e0ba77be76dea9b7aa5c9e97
Ok JaeOok <okjaeook98@gmail.com>
2024-01-07 20:39:24 +0900

    [Feat] Place Biz 구현 - Service
    `#` Biz 메시지 외 다른 요소 배재

--- app/src/main/java/fc/be/app/domain/place/service/PlaceService.java
+++ app/src/main/java/fc/be/app/domain/place/service/PlaceService.java
@@ -1,36 +1,54 @@
 package fc.be.app.domain.place.service;
 
-import fc.be.openapi.tourapi.dto.bone.*;
+import fc.be.app.domain.place.dto.PlaceInfoGetResponse;
+import fc.be.app.domain.place.dto.PlaceInfoInsertRequest;
+import fc.be.app.domain.place.exception.PlaceException;
+import fc.be.app.domain.place.repository.PlaceRepository;
 import fc.be.openapi.tourapi.TourAPIService;
-import fc.be.openapi.tourapi.constant.ContentTypeId;
+import fc.be.openapi.tourapi.dto.bone.PlaceDTO;
 import fc.be.openapi.tourapi.tools.ObjectMerger;
+import fc.be.openapi.tourapi.tools.TourAPIDomainConverter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import static fc.be.app.domain.place.exception.PlaceErrorCode.PLACE_NOT_LOADED;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PlaceService {
     private final TourAPIService tourAPIService;
+    private final TourAPIDomainConverter tourAPIDomainConverter;
+    private final PlaceRepository placeRepository;
 
-    public PlaceDTO bringPlaceInfo(final int placeId, final int contentTypeId) {
-        Class<? extends PlaceDTO> placeChildClass = getPlaceClass(contentTypeId);
+    public PlaceInfoGetResponse bringPlaceInfo(final int placeId, final int contentTypeId) {
+        Class<? extends PlaceDTO> placeChildClass = tourAPIDomainConverter.convertPlaceToChildDomain(contentTypeId);
 
-        PlaceDTO detail = tourAPIService.bringDetailCommonDomain(placeId, contentTypeId, placeChildClass);
-        PlaceDTO images = tourAPIService.bringDetailImageDomains(placeId, placeChildClass);
-        PlaceDTO intro = tourAPIService.bringDetailIntroDomain(placeId, contentTypeId, placeChildClass);
+        PlaceDTO place = ObjectMerger.merge(placeChildClass,
+                tourAPIService.bringDetailCommonDomain(placeId, contentTypeId),
+                tourAPIService.bringDetailImageDomains(placeId, contentTypeId),
+                tourAPIService.bringDetailIntroDomain(placeId, contentTypeId)
+        );
 
-        return ObjectMerger.merge(placeChildClass, detail, images, intro);
+        if (place == null) {
+            throw new PlaceException(PLACE_NOT_LOADED);
+        }
+
+        return new PlaceInfoGetResponse(place);
     }
 
-    private Class<? extends PlaceDTO> getPlaceClass(int contentTypeId) {
-        return switch (ContentTypeId.of(contentTypeId)) {
-            case SPOT -> SpotDTO.class;
-            case FACILITY -> FacilityDTO.class;
-            case FESTIVAL -> FestivalDTO.class;
-            case LEPORTS -> LeportsDTO.class;
-            case ACCOMMODATION -> AccommodationDTO.class;
-            case SHOP -> ShopDTO.class;
-            case RESTAURANT -> RestaurantDTO.class;
-        };
+    @Transactional
+    public boolean insertPlaceInfo(int placeId, PlaceInfoInsertRequest placeInfoInsertRequest) {
+        if (placeRepository.existsById(placeId)) {
+            log.info("이미 존재하는 placeId 입니다. Mysql DB에 저장되진 않습니다");
+            return false;
+        }
+
+        placeRepository.save(placeInfoInsertRequest.to(placeId));
+        return true;
     }
+
 }
