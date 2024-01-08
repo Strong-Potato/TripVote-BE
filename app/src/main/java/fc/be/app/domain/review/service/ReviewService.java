bb175d6364023a2b33b3afb58f8cf05d69684d98
happymink <minkyun@kakao.com>
2024-01-08 18:20:38 +0900

    [Refactor] 리뷰 생성 시 `Place`를 저장하는 로직 추가
    `#` createReview 메서드 내부 로직

--- app/src/main/java/fc/be/app/domain/review/service/ReviewService.java
+++ app/src/main/java/fc/be/app/domain/review/service/ReviewService.java
@@ -1,7 +1,13 @@
 package fc.be.app.domain.review.service;
 
+import fc.be.app.domain.member.entity.Member;
+import fc.be.app.domain.member.entity.MemberRepository;
+import fc.be.app.domain.place.dto.PlaceInfoInsertRequest;
+import fc.be.app.domain.place.service.PlaceService;
 import fc.be.app.domain.review.dto.*;
 import fc.be.app.domain.review.entity.Review;
+import fc.be.app.domain.review.exception.ReviewErrorCode;
+import fc.be.app.domain.review.exception.ReviewException;
 import fc.be.app.domain.review.repository.ReviewRepository;
 import fc.be.app.global.http.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,18 +20,26 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
+    private final MemberRepository memberRepository;
+    private final PlaceService placeService;
 
     @Transactional
     public ReviewCreateResponse createReview(ReviewCreateRequest reviewCreateRequest) {
         //todo [Review] Security 적용 -1
+        var member = memberRepository.save(memberRepository.save(Member.builder().build()));
+        placeService.insertPlaceInfo(reviewCreateRequest.placeId(), new PlaceInfoInsertRequest(
+                reviewCreateRequest.contentTypeId(), reviewCreateRequest.title(),
+                reviewCreateRequest.placeDTO().getLocationDTO(), reviewCreateRequest.placeDTO().getThumbnail())
+        );
+
         return ReviewCreateResponse.from(reviewRepository.save(
-                reviewCreateRequest.to(reviewCreateRequest, null)));
+                reviewCreateRequest.to(member)));
     }
 
     @Transactional
     public ApiResponse<ReviewEditResponse> editReview(ReviewEditRequest reviewEditRequest) {
         Review review = reviewRepository.findById(reviewEditRequest.reviewId())
-                .orElseThrow(() -> new RuntimeException());
+                .orElseThrow(() -> new ReviewException(ReviewErrorCode.REVIEW_NOT_FOUND));
 
         review.editReview(reviewEditRequest);
         return ApiResponse.ok(ReviewEditResponse.from(reviewRepository.save(review)));
