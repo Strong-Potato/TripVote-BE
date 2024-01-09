b30f6a82ac419c94f23e1c1c850f98fc7970919d
NOJaeHyuk <jaehyuk7722@naver.com>
2024-01-09 23:24:44 +0900

    [Chore] QueryDSL,Security,Validation 설정 코드추가
    `+` QueryDslConfig
    `+` SecurityConfig
    `+` application-test.yml
    `+` build.gradle : spring-boot-starter-validation

--- app/src/main/java/fc/be/TripVoteApplication.java
+++ app/src/main/java/fc/be/TripVoteApplication.java
@@ -1,9 +1,17 @@
 package fc.be;
 
+import fc.be.app.domain.member.entity.Member;
+import fc.be.app.domain.member.repository.MemberRepository;
 import fc.be.openapi.OpenAPIRoot;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication(scanBasePackageClasses = {
         OpenAPIRoot.class,
         NotificationRoot.class,
@@ -15,4 +23,18 @@ public class TripVoteApplication {
         SpringApplication.run(TripVoteApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner run(MemberRepository memberRepository) {
+        log.info("[Member 가 추가됨]");
+
+        return (String[] args) -> {
+            Member member = Member.builder()
+                .email("ok@gmail.com")
+                .nickname("ok")
+                .password("1234")
+                .build();
+
+            memberRepository.save(member);
+        };
+    }
 }
