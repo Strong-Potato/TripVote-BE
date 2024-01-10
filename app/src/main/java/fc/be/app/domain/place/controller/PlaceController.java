a7a48b0566323b416039208fa71f81696433f75e
Ok JaeOok <okjaeook98@gmail.com>
2024-01-10 19:11:23 +0900

    [Refactor] PlaceController 수정 - 장소 정보 저장 기능 삭제
    `-` 사용되지 않음. service에서 부가 동작하도록 수행

--- app/src/main/java/fc/be/app/domain/place/controller/PlaceController.java
+++ app/src/main/java/fc/be/app/domain/place/controller/PlaceController.java
@@ -1,9 +1,10 @@
 package fc.be.app.domain.place.controller;
 
-import fc.be.app.domain.place.dto.*;
+import fc.be.app.domain.place.dto.PlaceInfoGetResponse;
+import fc.be.app.domain.place.dto.PlacePopularGetResponse;
+import fc.be.app.domain.place.dto.PlaceSearchResponse;
 import fc.be.app.domain.place.service.PlaceService;
 import fc.be.app.global.http.ApiResponse;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,16 +24,6 @@ public class PlaceController {
                         placeId,
                         contentTypeId
                 )
-        );
-    }
-
-    @PostMapping("/{placeId}")
-    public ApiResponse<PlaceInfoInsertResponse> savePlaceInfo(
-            @PathVariable int placeId,
-            @Valid @RequestBody PlaceInfoInsertRequest placeInfoInsertRequest
-    ) {
-        return ApiResponse.ok(
-                placeService.insertPlaceInfo(placeId, placeInfoInsertRequest)
         );
     }
 
