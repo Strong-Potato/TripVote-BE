29219b2caeaef4d5a00447a23b45d1a80508ea4b
Denny-Ko <donghar@naver.com>
2024-01-11 19:14:43 +0900

    [Fix] 컨트롤러 테스트에서 Spring security 의존성 제거

--- app/src/test/java/fc/be/app/domain/space/controller/SpaceControllerTest.java
+++ app/src/test/java/fc/be/app/domain/space/controller/SpaceControllerTest.java
@@ -1,33 +1,43 @@
 package fc.be.app.domain.space.controller;
 
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fc.be.app.domain.space.dto.request.UpdateSpaceRequest.DateUpdateRequest;
 import fc.be.app.domain.space.dto.request.UpdateSpaceRequest.TitleUpdateRequest;
 import fc.be.app.domain.space.dto.response.SpaceResponse;
 import fc.be.app.domain.space.service.SpaceService;
+import fc.be.app.global.config.security.SecurityAppConfig;
 import fc.be.app.global.config.security.SecurityConfig;
-import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = SpaceController.class)
-@Import(SecurityConfig.class)
+import java.time.LocalDate;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = SpaceController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {SecurityConfig.class, SecurityAppConfig.class})
+        },
+        excludeAutoConfiguration = {
+                SecurityAutoConfiguration.class,
+                OAuth2ClientAutoConfiguration.class,
+        }
+)
 class SpaceControllerTest {
 
     @Autowired
@@ -47,10 +57,10 @@ class SpaceControllerTest {
     void createSpace() throws Exception {
         // when then
         mockMvc.perform(
-                post("/spaces")
-            )
-            .andDo(print())
-            .andExpect(status().isOk());
+                        post("/spaces")
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @DisplayName("여행스페이스 식별자값으로 여행스페이스 값을 조회한다.")
@@ -58,20 +68,20 @@ class SpaceControllerTest {
     void getSpaceById() throws Exception {
         // given
         SpaceResponse spaceResponse = SpaceResponse.builder()
-            .title("부산 여행")
-            .build();
+                .title("부산 여행")
+                .build();
         when(spaceService.getSpaceById(anyLong())).thenReturn(spaceResponse);
 
         // when then
         Long spaceId = 1L;
         mockMvc.perform(
-                get("/spaces/{spaceId}", spaceId)
-                    .contentType(MediaType.APPLICATION_JSON)
-            )
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.status").value("200"))
-            .andExpect(jsonPath("$.message").value("SUCCESS"))
-            .andExpect(jsonPath("$.data.title").value("부산 여행"));
+                        get("/spaces/{spaceId}", spaceId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("200"))
+                .andExpect(jsonPath("$.message").value("SUCCESS"))
+                .andExpect(jsonPath("$.data.title").value("부산 여행"));
     }
 
     @DisplayName("여행스페이스의 타이틀을 수정한다.")
@@ -79,17 +89,17 @@ class SpaceControllerTest {
     void updateSpaceByTitle() throws Exception {
         // given
         TitleUpdateRequest updateRequest = TitleUpdateRequest.builder()
-            .title("서울여행")
-            .build();
+                .title("서울여행")
+                .build();
 
         // when then
         mockMvc.perform(
-                put("/spaces/1/title")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(updateRequest))
-            )
-            .andDo(print())
-            .andExpect(status().isOk());
+                        put("/spaces/1/title")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(updateRequest))
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @DisplayName("여행스페이스의 타이틀의 값이 넘어오지 않은 경우 예외를 발생시킨다.")
@@ -97,20 +107,20 @@ class SpaceControllerTest {
     void updateSpaceByTitleWithoutTitle() throws Exception {
         // given
         TitleUpdateRequest updateRequest = TitleUpdateRequest.builder()
-            .build();
+                .build();
 
         // when then
         mockMvc.perform(
-                put("/spaces/1/title")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(updateRequest))
-            )
-            .andDo(print())
-            .andExpect(status().isBadRequest())
-            .andExpect(jsonPath("$.title").value("Bad Request"))
-            .andExpect(jsonPath("$.status").value(400))
-            .andExpect(jsonPath("$.detail").value("Invalid request content."))
-            .andExpect(jsonPath("$.instance").value("/spaces/1/title"));
+                        put("/spaces/1/title")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(updateRequest))
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.title").value("Bad Request"))
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.detail").value("Invalid request content."))
+                .andExpect(jsonPath("$.instance").value("/spaces/1/title"));
     }
 
     @DisplayName("여행스페이스의 날짜값을 수정한다.")
@@ -118,18 +128,18 @@ class SpaceControllerTest {
     void updateSpaceByDates() throws Exception {
         // given
         DateUpdateRequest updateRequest = DateUpdateRequest.builder()
-            .startDate(LocalDate.of(2024, 1, 3))
-            .endDate(LocalDate.of(2024, 1, 5))
-            .build();
+                .startDate(LocalDate.of(2024, 1, 3))
+                .endDate(LocalDate.of(2024, 1, 5))
+                .build();
 
         // when then
         mockMvc.perform(
-                put("/spaces/1/dates")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(updateRequest))
-            )
-            .andDo(print())
-            .andExpect(status().isOk());
+                        put("/spaces/1/dates")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(updateRequest))
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @DisplayName("여행스페이스의 날짜값이 넘어오지 않은 경우 예외를 발생시킨다.")
@@ -137,19 +147,19 @@ class SpaceControllerTest {
     void updateSpaceByDatesWithoutDates() throws Exception {
         // given
         DateUpdateRequest updateRequest = DateUpdateRequest.builder()
-            .build();
+                .build();
 
         // when then
         mockMvc.perform(
-                put("/spaces/1/dates")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(updateRequest))
-            )
-            .andDo(print())
-            .andExpect(status().isBadRequest())
-            .andExpect(jsonPath("$.title").value("Bad Request"))
-            .andExpect(jsonPath("$.status").value(400))
-            .andExpect(jsonPath("$.detail").value("Invalid request content."))
-            .andExpect(jsonPath("$.instance").value("/spaces/1/dates"));
+                        put("/spaces/1/dates")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(updateRequest))
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.title").value("Bad Request"))
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.detail").value("Invalid request content."))
+                .andExpect(jsonPath("$.instance").value("/spaces/1/dates"));
     }
 }
