86a36c5d4b659565bce912e5cef5795b952c329e
Denny-Ko <donghar@naver.com>
2024-01-11 17:44:25 +0900

    [Feat] Anonymous 유저 및 권한 없는 유저 핸들링
    `+` 로그인 되어있지만 권한이 없는 유저: RestAccessDeniedHandler
    `+` 로그인 되어있지 않은 유저: RestAuthenticationEntryPoint
    `#` GlobalExceptionHandler

--- app/src/main/java/fc/be/app/global/exception/GlobalExceptionHandler.java
+++ app/src/main/java/fc/be/app/global/exception/GlobalExceptionHandler.java
@@ -7,10 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -26,15 +28,30 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     private static final URI type = URI.create("https://tripvote.site/error");
 
     @ExceptionHandler
+    public ResponseEntity handleException(AccessDeniedException ex, WebRequest request) {
+        HttpStatus httpStatus = HttpStatus.FORBIDDEN;
+        String errorMessage = "Not permitted request";
+        ProblemDetail problemDetail = createProblemDetail(ex, httpStatus, errorMessage, null, null, request);
+        problemDetail.setType(type);
+        return handleExceptionInternal(ex, problemDetail, new HttpHeaders(), httpStatus, request);
+    }
+
+    @ExceptionHandler
     public ResponseEntity handleException(AuthenticationException ex, WebRequest request) {
         HttpStatus httpStatus = HttpStatus.UNAUTHORIZED;
         String errorMessage = "Authentication Failed";
-        if (ex instanceof BadCredentialsException) {
-            errorMessage = "Invalid Username Or Password";
-        } else if (ex instanceof InsufficientAuthenticationException) {
-            errorMessage = "Invalid Secret Key";
-        } else if (ex instanceof UsernameNotFoundException) {
-            errorMessage = ex.getMessage();
+        if (ex instanceof OAuth2AuthenticationException exception) {
+            log.warn("[OAUTH_AUTHENTICATION_EXCEPTION] OAUTH_AUTHENTICATION FAILED : {}", ex.getCause());
+            errorMessage = exception.getMessage();
+        } else if (ex instanceof AuthenticationException exception) {
+            log.warn("[AUTHENTICATION_EXCEPTION] AUTHENTICATION FAILED : {}", ex.getCause());
+            if (exception instanceof BadCredentialsException) {
+                errorMessage = "이메일 또는 비밀번호를 확인해주세요.";
+            } else if (exception instanceof InsufficientAuthenticationException) {
+                errorMessage = "로그인을 먼저 해주세요.";
+            } else if (exception instanceof UsernameNotFoundException) {
+                errorMessage = "회원가입되지 않은 이메일입니다.";
+            }
         }
         ProblemDetail problemDetail = createProblemDetail(ex, httpStatus, errorMessage, null, null, request);
         problemDetail.setType(type);
