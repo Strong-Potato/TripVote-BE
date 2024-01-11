09de8d9693ca9047e51e2e7cb7f12841785723d0
Denny-Ko <donghar@naver.com>
2024-01-11 18:07:31 +0900

    [Chore] TODO 주석 제거

--- app/src/main/java/fc/be/app/global/config/security/handler/LoginAuthenticationSuccessHandler.java
+++ app/src/main/java/fc/be/app/global/config/security/handler/LoginAuthenticationSuccessHandler.java
@@ -12,11 +12,6 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 
 import java.io.IOException;
 
-// TODO: 세션은 토큰(Access-token, Refresh-token)으로 유지하기로 한다.
-// TODO: code를 가지고 여행스페이스에 접근하는 요청의 경우,
-//  Session(Redis)를 발급해서 그 안에 초대받은 여행스페이스 정보를 가지고 있을 것이고,
-//  인증이 성공했을 때, SSE로 발송해주어야 한다.
-// TODO: AbstractAuthenticationTargetUrlRequestHandler, RedirectAttributes, RequestMappingHandlerAdapter.handleInternal(),
 public class LoginAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
