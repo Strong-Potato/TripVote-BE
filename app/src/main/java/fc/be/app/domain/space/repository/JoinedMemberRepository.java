8b7fd7789bea4a7b61605b3a6ba49241bfa91df8
NOJaeHyuk <jaehyuk7722@naver.com>
2024-01-12 00:39:04 +0900

    [Feat] Journey 기능 구현 - Repository
    `+` JourneyRepository
    `+` JoinedMemberRepository

--- app/src/main/java/fc/be/app/domain/space/repository/JoinedMemberRepository.java
+++ app/src/main/java/fc/be/app/domain/space/repository/JoinedMemberRepository.java
@@ -1,8 +1,12 @@
 package fc.be.app.domain.space.repository;
 
+import fc.be.app.domain.member.entity.Member;
 import fc.be.app.domain.space.entity.JoinedMember;
+import fc.be.app.domain.space.entity.Space;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JoinedMemberRepository extends JpaRepository<JoinedMember, Long> {
 
+    Optional<JoinedMember> findBySpaceAndMember(Space space, Member member);
 }
