ed66e975cd6a82890a088c9152ff1ab320565bfb
Denny-Ko <donghar@naver.com>
2024-01-12 23:40:06 +0900

    [Fix] Lombok 사용 및 serialVersionUID 삭제

--- app/src/main/java/fc/be/app/global/config/security/model/token/RefreshToken.java
+++ app/src/main/java/fc/be/app/global/config/security/model/token/RefreshToken.java
@@ -1,40 +1,20 @@
 package fc.be.app.global.config.security.model.token;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class RefreshToken implements Serializable {
-    private static final long serialVersionUID = 1L;
-
     private String accessToken;
     private Long userId;
     private String clientIp;
     private String userAgent;
     private String value;
-
-    public RefreshToken(String accessToken, Long userId, String clientIp, String userAgent, String value) {
-        this.accessToken = accessToken;
-        this.userId = userId;
-        this.clientIp = clientIp;
-        this.userAgent = userAgent;
-        this.value = value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RefreshToken that = (RefreshToken) o;
-        return Objects.equals(accessToken, that.accessToken) && Objects.equals(userId, that.userId) && Objects.equals(clientIp, that.clientIp) && Objects.equals(userAgent, that.userAgent) && Objects.equals(value, that.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(accessToken, userId, clientIp, userAgent, value);
-    }
 }
