cf83f609602b4aa33e95e6286717e7c874f784ef
NOJaeHyuk <jaehyuk7722@naver.com>
2024-01-13 22:18:11 +0900

    [Refactor] Code 스타일 적용(Intellij 버전 이슈로 따로 커밋)

--- app/src/main/java/fc/be/app/domain/space/repository/SelectedPlaceRepository.java
+++ app/src/main/java/fc/be/app/domain/space/repository/SelectedPlaceRepository.java
@@ -2,7 +2,9 @@ package fc.be.app.domain.space.repository;
 
 import fc.be.app.domain.space.entity.Journey;
 import fc.be.app.domain.space.entity.SelectedPlace;
+
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
