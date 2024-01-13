212ae8c9468cacc762dfc4f0ac9e424c9be0f432
NOJaeHyuk <jaehyuk7722@naver.com>
2024-01-13 15:07:47 +0900

    [Fix] DTO 레코드로 변경 및 List 변수형으로 변경
    `+` DateUpdateRequest
    `+` JourneyResponse, JourneysResponse
    `+` TitleUpdateRequest
    `+` SpaceResponse, SpacesResponse
    `-` UpdateSpaceRequest

---
+++ app/src/main/java/fc/be/app/domain/space/dto/response/JourneysResponse.java
@@ -0,0 +1,16 @@
+package fc.be.app.domain.space.dto.response;
+
+import fc.be.app.domain.space.entity.Journey;
+import java.util.List;
+
+
+public record JourneysResponse(
+    List<JourneyResponse> journeyResponses
+) {
+    public static JourneysResponse from(List<Journey> journeys) {
+        List<JourneyResponse> journeyResponses = journeys.stream()
+            .map(JourneyResponse::from)
+            .toList();
+        return new JourneysResponse(journeyResponses);
+    }
+}
