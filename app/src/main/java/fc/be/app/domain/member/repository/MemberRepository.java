b64d9a30db01da8a82c6de9646ce5a9533cd80d4
Denny-Ko <donghar@naver.com>
2024-01-14 18:59:13 +0900

    [Test] MemberQueryHandler, MemberCommandHandler, MemberRepository 테스트 코드 작성

--- app/src/main/java/fc/be/app/domain/member/repository/MemberRepository.java
+++ app/src/main/java/fc/be/app/domain/member/repository/MemberRepository.java
@@ -8,8 +8,20 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    /**
+     * <strong>email이 unique하지 않음을 주의!</strong>
+     *
+     * @param email 찾고자 하는 유저의 이메일
+     * @return 해당 이메일을 가진 유저들
+     */
     List<Member> findByEmail(String email);
 
+    /**
+     * <strong>nickname이 unique하지 않음을 주의!</strong>
+     *
+     * @param nickname 찾고자 하는 유저의 닉네임
+     * @return 해당 닉네임을 가진 유저들
+     */
     List<Member> findByNickname(String nickname);
 
     Optional<Member> findByProviderAndEmail(AuthProvider provider, String email);
