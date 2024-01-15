3b0a0dcb44b2f1e89a8defee87552f914de6462a
NOJaeHyuk <jaehyuk7722@naver.com>
2024-01-15 16:39:43 +0900

    [Feat] SpaceResponse에 JoinedMember 추가
    `+` MemberInfo

--- app/src/main/java/fc/be/app/domain/space/dto/response/SpaceResponse.java
+++ app/src/main/java/fc/be/app/domain/space/dto/response/SpaceResponse.java
@@ -1,25 +1,49 @@
 package fc.be.app.domain.space.dto.response;
 
+import fc.be.app.domain.space.entity.JoinedMember;
 import fc.be.app.domain.space.entity.Space;
+import lombok.Builder;
 
 import java.time.LocalDate;
-
-import lombok.Builder;
+import java.util.ArrayList;
+import java.util.List;
 
 @Builder
 public record SpaceResponse(
         Long id,
         String title,
         LocalDate startDate,
-        LocalDate endDate
+        LocalDate endDate,
+        List<MemberInfo> members
 ) {
     public static SpaceResponse of(Space space) {
+
+        List<MemberInfo> memberInfos = new ArrayList<>();
+
+        for (JoinedMember joinedMember : space.getJoinedMembers()) {
+            memberInfos.add(MemberInfo.builder()
+                    .id(joinedMember.getId())
+                    .nickname(joinedMember.getMember().getNickname())
+                    .profile(joinedMember.getMember().getProfile())
+                    .build());
+        }
+
         return
                 SpaceResponse.builder()
                         .id(space.getId())
                         .title(space.getTitle())
                         .startDate(space.getStartDate())
                         .endDate(space.getEndDate())
+                        .members(memberInfos)
                         .build();
+    }
+
+    @Builder
+    private record MemberInfo(
+            Long id,
+            String nickname,
+            String profile
+    ) {
+
     }
 }
