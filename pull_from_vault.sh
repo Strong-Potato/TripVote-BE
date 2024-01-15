6cbb35be97935cc4c2d15c1423962bf0b9e61175
Nine-JH <rnwkgus2@gmail.com>
2024-01-16 00:21:49 +0900

    [Feat] key 전용 서브모듈 추가
    motivation:
    - git level에서 key 들을 관리하기 위해 서브모듈을 추가합니다.
    
    modification:
    - `vault` 추가
    - submodule을 load 하는 script 추가
    
    # Close 92

---
+++ pull_from_vault.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+echo "
+ _        _                 _         |
+| |      (_)               | |        |
+| |_ _ __ _ _ ____   _____ | |_ ___   |
+| __| '__| | '_ \ \ / / _ \| __/ _ \  |
+| |_| |  | | |_) \ V / (_) | ||  __/  |
+ \__|_|  |_| .__/ \_/ \___/ \__\___|  |
+           | |                        |
+           |_|                        |
+"
+
+
+echo "update application profiles from boocam-be-vaults..."
+git submodule init
+git submodule foreach git pull origin main
+
+echo "done!"
+
+echo "remove old application profiles"
+rm ./app/src/main/resources/application-common.yml
+rm ./app/src/main/resources/application-prod.yml
+
+rm ./openapi/src/main/resources/application-openapi.yml
+
+echo "copy application profiles to application resources path"
+cp ./vault/app/* ./app/src/main/resources
+cp ./vault/openapi/* ./openapi/src/main/resources
