d323a5f9dd5b716b883050e673ecb452ffa5c8d5
Nine-JH <rnwkgus2@gmail.com>
2024-01-15 10:31:32 +0900

    [feat] 투표 후보 추가 기능 추가 motivation: - 스페이스 회원들이 투표 후보를 추가하는 기능을 구현합니다. - 단건 뿐 아니라 리스트로 추가가 가능합니다.
    modification:
    - 후보 추가 기능 구현.

--- app/src/main/java/fc/be/app/domain/vote/service/service/VoteManageService.java
+++ app/src/main/java/fc/be/app/domain/vote/service/service/VoteManageService.java
@@ -3,33 +3,50 @@ package fc.be.app.domain.vote.service.service;
 import fc.be.app.domain.member.entity.Member;
 import fc.be.app.domain.member.exception.MemberException;
 import fc.be.app.domain.member.repository.MemberRepository;
+import fc.be.app.domain.place.Place;
+import fc.be.app.domain.place.repository.PlaceRepository;
 import fc.be.app.domain.space.entity.Space;
 import fc.be.app.domain.space.exception.SpaceException;
 import fc.be.app.domain.space.repository.SpaceRepository;
+import fc.be.app.domain.vote.dto.MemberProfile;
+import fc.be.app.domain.vote.entity.Candidate;
 import fc.be.app.domain.vote.entity.Vote;
+import fc.be.app.domain.vote.exception.VoteErrorCode;
+import fc.be.app.domain.vote.exception.VoteException;
 import fc.be.app.domain.vote.repository.VoteRepository;
 import fc.be.app.domain.vote.service.VoteManageUseCase;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 import static fc.be.app.domain.member.exception.MemberErrorCode.MEMBER_NOT_FOUND;
 import static fc.be.app.domain.space.exception.SpaceErrorCode.*;
+import static fc.be.app.domain.vote.exception.VoteErrorCode.CANDIDATE_IS_MAX;
+import static fc.be.app.domain.vote.service.VoteInfoQueryUseCase.CandidateResponse;
+import static fc.be.app.domain.vote.service.VoteInfoQueryUseCase.VoteResponse;
+import static fc.be.app.domain.vote.service.VoteManageUseCase.CandidateAddRequest.CandidateInfo;
 
 @Service
 public class VoteManageService implements VoteManageUseCase {
 
+    private static final int CANDIDATE_COUNT_THRESHOLD = 15;
+
     private final VoteRepository voteRepository;
     private final SpaceRepository spaceRepository;
     private final MemberRepository memberRepository;
+    private final PlaceRepository placeRepository;
 
     public VoteManageService(VoteRepository voteRepository,
                              SpaceRepository spaceRepository,
-                             MemberRepository memberRepository) {
+                             MemberRepository memberRepository,
+                             PlaceRepository placeRepository) {
         this.voteRepository = voteRepository;
         this.spaceRepository = spaceRepository;
         this.memberRepository = memberRepository;
+        this.placeRepository = placeRepository;
     }
 
     @Transactional
@@ -57,5 +74,61 @@ public class VoteManageService implements VoteManageUseCase {
         if (!space.isBelong(requestMember)) {
             throw new SpaceException(NOT_JOINED_MEMBER);
         }
+    }
+
+    @Override
+    public VoteResponse addCandidate(CandidateAddRequest request) {
+        final Member requestMember = memberRepository.findById(request.memberId())
+                .orElseThrow(() -> new MemberException(MEMBER_NOT_FOUND));
+
+        Vote vote = voteRepository.findById(request.voteId())
+                .orElseThrow(() -> new VoteException(VoteErrorCode.VOTE_NOT_FOUND));
+
+        final Space space = vote.getSpace();
+
+        validateSpace(space, requestMember);
+        validateVote(vote);
+
+        List<Integer> placeIds = extractPlaceIdsFromRequest(request);
+
+        List<Place> places = placeRepository.findAllById(placeIds);
+
+        for (Place place : places) {
+            Optional<String> first = findMatchTagline(request, place);
+
+            vote.addCandidate(Candidate.of(place, vote, first.orElseGet(() -> "")));
+        }
+
+        // TODO : Sending a new vote creation event to all members of the space
+        return new VoteResponse(
+                vote.getId(),
+                vote.getTitle(),
+                new MemberProfile(
+                        vote.getOwner().getId(),
+                        vote.getOwner().getNickname(),
+                        vote.getOwner().getProfile()
+                ),
+                vote.getCandidates().stream()
+                        .map(CandidateResponse::of)
+                        .toList());
+    }
+
+    private static void validateVote(Vote vote) {
+        if (vote.isMax(CANDIDATE_COUNT_THRESHOLD)) {
+            throw new VoteException(CANDIDATE_IS_MAX);
+        }
+    }
+
+    private List<Integer> extractPlaceIdsFromRequest(CandidateAddRequest request) {
+        return request.candidateInfo().stream()
+                .map(CandidateInfo::placeId)
+                .toList();
+    }
+
+    private Optional<String> findMatchTagline(CandidateAddRequest request, Place place) {
+        return request.candidateInfo().stream()
+                .filter(info -> info.placeId().equals(place.getId()))
+                .map(CandidateInfo::tagline)
+                .findFirst();
     }
 }
