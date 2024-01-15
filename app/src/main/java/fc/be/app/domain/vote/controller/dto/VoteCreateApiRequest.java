da0fa9f0b62113b6959f6aeacfbf5e798ede4566
Nine-JH <rnwkgus2@gmail.com>
2024-01-15 10:06:12 +0900

    [feat] 투표 생성 기능 추가 motivation: - spaceId는 항상 0보다 큰 값이기 때문에 @Positive 추가합니다.
    modification:
    - @Positive 추가

--- app/src/main/java/fc/be/app/domain/vote/controller/dto/VoteCreateApiRequest.java
+++ app/src/main/java/fc/be/app/domain/vote/controller/dto/VoteCreateApiRequest.java
@@ -1,10 +1,11 @@
 package fc.be.app.domain.vote.controller.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record VoteCreateApiRequest(
-        @NotNull Long spaceId,
+        @Positive @NotNull Long spaceId,
         @Size(min = 1, max = 15) String title
 ) {
 }
