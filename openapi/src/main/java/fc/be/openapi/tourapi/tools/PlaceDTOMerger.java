97b8911dffbc593749ffc2d64bb9608e9dc71fc6
Ok JaeOok <okjaeook98@gmail.com>
2024-01-15 15:34:40 +0900

    [Refactor] ObjectMerger 조정
    `#` 클래스 이름 변경
    `#` merge 매개변수 객체들끼리만 다룸

---
+++ openapi/src/main/java/fc/be/openapi/tourapi/tools/PlaceDTOMerger.java
@@ -0,0 +1,39 @@
+package fc.be.openapi.tourapi.tools;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import fc.be.openapi.tourapi.dto.bone.PlaceDTO;
+import fc.be.openapi.tourapi.exception.TourAPIError;
+import lombok.NoArgsConstructor;
+
+import static fc.be.openapi.tourapi.exception.TourAPIErrorCode.OBJECT_TYPE_ERROR;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public class PlaceDTOMerger {
+    private static final ObjectMapper objectMapper;
+
+    static {
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+    }
+
+    public static <T extends PlaceDTO> T merge(PlaceDTO target, PlaceDTO... updates) {
+        try {
+            ObjectNode targetNode = objectMapper.valueToTree(target);
+            for (PlaceDTO update : updates) {
+                JsonNode updateNode = objectMapper.valueToTree(update);
+                updateNode.fields().forEachRemaining(field -> {
+                    if (!field.getValue().isNull()) {
+                        targetNode.set(field.getKey(), field.getValue());
+                    }
+                });
+            }
+            return objectMapper.convertValue(targetNode, (Class<T>) target.getClass());
+        } catch (ClassCastException e) {
+            new TourAPIError(OBJECT_TYPE_ERROR);
+            return null;
+        }
+    }
+}
