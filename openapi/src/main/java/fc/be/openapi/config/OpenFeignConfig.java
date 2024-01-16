0a133fc98021aa021c2b86a053323697cd42c1db
happymink <minkyun@kakao.com>
2024-01-16 22:48:48 +0900

    [Refactor] OpenFeign Profile 설정
    `#` OpenFeign에 대한 Configuration이 배포환경에서만 동작합니다.

--- openapi/src/main/java/fc/be/openapi/config/OpenFeignConfig.java
+++ openapi/src/main/java/fc/be/openapi/config/OpenFeignConfig.java
@@ -2,7 +2,9 @@ package fc.be.openapi.config;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("prod")
 @Configuration
 @EnableFeignClients(basePackages = "fc.be.openapi")
 public class OpenFeignConfig {
