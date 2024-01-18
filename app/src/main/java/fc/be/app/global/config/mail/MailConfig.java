d2b85aede1d459775cf99311fb66704edd897ada
Denny-Ko <donghar@naver.com>
2024-01-19 00:04:09 +0900

    [Fix] test용 JavaMailSender 빈 등록

--- app/src/main/java/fc/be/app/global/config/mail/MailConfig.java
+++ app/src/main/java/fc/be/app/global/config/mail/MailConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
@@ -13,6 +14,7 @@ import java.util.Properties;
 @EnableConfigurationProperties(MailProperties.class)
 public class MailConfig {
     @Bean
+    @Profile({"!test"})
     public JavaMailSender javaMailSender(MailProperties mailProperties) {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
         Properties javaMailProperties = new Properties();
@@ -24,6 +26,13 @@ public class MailConfig {
         javaMailSender.setDefaultEncoding("utf-8");
         javaMailSender.setJavaMailProperties(javaMailProperties);
 
+        return javaMailSender;
+    }
+
+    @Bean
+    @Profile({"test"})
+    public JavaMailSender javaMailSenderForTest() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
         return javaMailSender;
     }
 }
