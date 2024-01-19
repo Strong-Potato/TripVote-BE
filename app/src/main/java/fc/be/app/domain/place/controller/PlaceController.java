d43457dcd09ec0fbf6dc7f7ea8f169fcd0223805
Ok JaeOok <okjaeook98@gmail.com>
2024-01-20 04:31:32 +0900

    [Feat] PlaceController 에 Algolia 반영
    `#` 장소 검색시 검색 기록을 저장
    `+` 인기 검색어 제공

--- app/src/main/java/fc/be/app/domain/place/controller/PlaceController.java
+++ app/src/main/java/fc/be/app/domain/place/controller/PlaceController.java
@@ -3,18 +3,19 @@ package fc.be.app.domain.place.controller;
 import fc.be.app.domain.place.dto.*;
 import fc.be.app.domain.place.service.PlaceService;
 import fc.be.app.global.http.ApiResponse;
+import fc.be.openapi.algolia.SearchEngineService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/places")
 @RestController
 @RequiredArgsConstructor
 public class PlaceController {
     private final PlaceService placeService;
+    private final SearchEngineService searchEngineService;
 
     @GetMapping("/info")
     public ApiResponse<PlaceInfoGetResponse> sendPlaceInfo(
@@ -27,6 +28,10 @@ public class PlaceController {
     public ApiResponse<PlaceSearchResponse> sendSearchKeywordResults(
             @Valid @ModelAttribute PlaceSearchRequest placeSearchRequest
     ) {
+        final String wildCard = "_";
+        if (!placeSearchRequest.keyword().equals(wildCard)) {
+            searchEngineService.saveSearchHistory(placeSearchRequest.keyword());
+        }
         return ApiResponse.ok(placeService.bringSearchKeywordResults(placeSearchRequest));
     }
 
@@ -42,5 +47,10 @@ public class PlaceController {
             @Valid @ModelAttribute PlacePopularGetRequest placePopularGetRequest
     ) {
         return ApiResponse.ok(placeService.bringPopularPlaces(placePopularGetRequest));
+    }
+
+    @GetMapping("/popular/keywords")
+    public ApiResponse<List<String>> sendPopularSearchKeywords(@RequestParam(defaultValue = "10") Integer size) {
+        return ApiResponse.ok(searchEngineService.bringPopularSearchKeywords(size));
     }
 }
