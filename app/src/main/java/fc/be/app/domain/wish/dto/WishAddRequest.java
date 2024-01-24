95ab36a29b0c51d84480791d4a65b980d845a61b
Ok JaeOok <okjaeook98@gmail.com>
2024-01-24 17:45:55 +0900

    [Refactor] Wish 찜 추가 DTO 수정
    `-` memberId 를 `UserPrincipal` 에서 받아옴

--- app/src/main/java/fc/be/app/domain/wish/dto/WishAddRequest.java
+++ app/src/main/java/fc/be/app/domain/wish/dto/WishAddRequest.java
@@ -3,7 +3,6 @@ package fc.be.app.domain.wish.dto;
 import jakarta.validation.constraints.Positive;
 
 public record WishAddRequest(
-        @Positive Long memberId,
         @Positive Integer placeId,
         @Positive Integer contentTypeId
 ) {
